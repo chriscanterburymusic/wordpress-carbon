bda255e9b1dd260145a0fb4572d8586479564e86
Kaleb Heitzman <jkheitzman@gmail.com>
2013-07-28 21:08:01 -0400

    added foundation

---
+++ navbar.php
@@ -0,0 +1,12 @@
+<div class="contain-to-grid">
+	<nav class="top-bar">
+		<ul class="title-area">
+			<li class="name"><h1><a href="<?php echo get_bloginfo('url'); ?>"><?php echo get_bloginfo('name'); ?></a></h1></li>
+			<li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
+		</ul>
+		<section class="top-bar-section">
+			<?php foundation_top_bar_left(); ?>
+			<?php // foundation_top_bar_right(); ?>
+		</section><!--.top-bar-section-->
+	</nav>
+</div>
